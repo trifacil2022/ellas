29aabfaf6dfe8e7dcea60be6e4c3322c713839ff
trifacil2022 <trifacil.programacion@gmail.com>
2023-04-09 19:11:05 -0600

    first commitc

---
+++ app/Models/Compra.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Compra extends Model
+{
+    use HasFactory;
+    public function CompraInventarios(){
+        return $this->hasMany(CompraInventario::class);
+    }
+}
---
+++ app/Models/Inventario.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Inventario extends Model
+{
+    use HasFactory;
+    public function Articulo(){
+        return $this->belongsTo(Articulo::class);
+    }
+}
